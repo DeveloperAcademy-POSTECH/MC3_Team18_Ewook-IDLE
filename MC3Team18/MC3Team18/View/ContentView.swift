7de42cdb35486c0e481eb54777cb75f54204649f
chad0909@naver.com <108044639+chad0909@users.noreply.github.com>
2023-07-11 11:59:43 +0900

    Feat/#9: 생성 기능 구현을 위한 더미버튼 2개

--- MC3Team18/MC3Team18/View/ContentView.swift
+++ MC3Team18/MC3Team18/View/ContentView.swift
@@ -10,10 +10,24 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Button {
+                //풍선껌 불기 View 넣어주시면 됩니다
+                EmptyView()
+            } label: {
+                Text("풍선껌 불기")
+            }
+            .padding(50)
+            .border(Color.red)
+            
+            Button {
+                //차곡차곡 View 넣어주시면 됩니다
+                EmptyView()
+            } label: {
+                Text("차곡차곡")
+            }
+            .padding(50)
+            .border(Color.red)
+
         }
         .padding()
     }
