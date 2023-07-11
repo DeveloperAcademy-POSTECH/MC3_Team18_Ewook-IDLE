b83fe6cad62274c3493b0cdeef4aae5f7188d349
dayjack <frostjanna12@gmail.com>
2023-07-11 17:12:45 +0900

    UI/#8: 수정 Chagok 관련 뷰 UI 수정, 스타일 적용 안함

--- MC3Team18/MC3Team18/Model/ChagokGameOverView.swift
+++ MC3Team18/MC3Team18/Model/ChagokGameOverView.swift
@@ -9,7 +9,61 @@ import SwiftUI
 
 struct ChagokGameOverView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ChagokView()
+            Color.black.opacity(0.5).ignoresSafeArea()
+            VStack {
+                Spacer().frame(height: 157)
+                Text("Your Score")
+                    .padding(.bottom, 40)
+                    .fontWeight(.light)
+                    .font(.system(size: 32))
+                Text("140")
+                    .fontWeight(.regular)
+                    .font(.system(size: 64))
+                    .padding(.bottom, 40)
+                HStack {
+                    Text("Best Score")
+                        .fontWeight(.regular)
+                        .font(.system(size: 24))
+                    Text("140")
+                        .fontWeight(.semibold)
+                        .font(.system(size: 24))
+                }
+                Spacer()
+                HStack(spacing: 60) {
+                    Button {
+                        
+                    } label: {
+                        Image("GameOverBtn")
+                            .overlay {
+                                VStack(spacing: 17) {
+                                    Image(systemName: "house")
+                                    Text("Home")
+                                        .fontWeight(.bold)
+                                        .font(.system(size: 24))
+                                }
+                            }
+                    }
+                    Button {
+                        
+                    } label: {
+                        Image("GameOverBtn")
+                            .overlay {
+                                VStack(spacing: 17) {
+                                    Image(systemName: "arrow.clockwise")
+                                    Text("Retry")
+                                        .fontWeight(.bold)
+                                        .font(.system(size: 24))
+                                }
+                            }
+                    }
+                }
+                .padding(.bottom, 160)
+            }
+            .ignoresSafeArea()
+            .foregroundColor(.white)
+        }
     }
 }
 
--- MC3Team18/MC3Team18/Model/ChagokTutorialView.swift
+++ MC3Team18/MC3Team18/Model/ChagokTutorialView.swift
@@ -9,7 +9,47 @@ import SwiftUI
 
 struct ChagokTutorialView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack {
+            ChagokView()
+            Color.black.opacity(0.5)
+                .ignoresSafeArea()
+            VStack {
+                HStack {
+                    Spacer()
+                    Image(systemName: "x.circle")
+                        .resizable()
+                        .frame(width: 36, height: 36)
+                }
+                .padding(.top, 26)
+                .padding(.trailing, 27)
+                Text("차곡차곡")
+                    .foregroundColor(.yellow)
+                    .fontWeight(.semibold)
+                    .font(.system(size: 36))
+                    .padding(.top, 94)
+                    .padding(.bottom, 45)
+                Text("컵에 있는 입모양대로\n내 입을 움직여서\n아래에서 부터 차곡차곡 쌓아보세요!")
+                    .fontWeight(.semibold)
+                    .font(.system(size: 20))
+                    .multilineTextAlignment(.center)
+                Spacer()
+                HStack {
+                    Image(systemName: "checkmark.square")
+                        .resizable()
+                        .frame(width: 29, height: 29)
+                    Text("다시보지않기")
+                        .fontWeight(.regular)
+                        .font(.system(size: 20))
+                        
+                }
+                .padding(.bottom, 36)
+                
+                
+            }
+            .foregroundColor(.white)
+            .ignoresSafeArea()
+            
+        }
     }
 }
 
