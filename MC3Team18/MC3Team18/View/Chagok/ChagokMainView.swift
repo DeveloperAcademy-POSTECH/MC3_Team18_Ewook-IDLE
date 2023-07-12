530f83e79e93a352ff1c902f624fb09f56c2c66e
dayjack <frostjanna12@gmail.com>
2023-07-12 17:09:58 +0900

    Feat/#36 생성 ChagokMainView, ChagokPauseView (UI 없이), 구현 ChagokStatus

---
+++ MC3Team18/MC3Team18/View/Chagok/ChagokMainView.swift
@@ -0,0 +1,41 @@
+//
+//  ChagokMainView.swift
+//  MC3Team18
+//
+//  Created by ChoiYujin on 2023/07/12.
+//
+
+import SwiftUI
+
+struct ChagokMainView: View {
+    
+    @Binding var gameSelection: GameSelection
+    @State private var chagokStatus: ChagokStatus = .tutorial
+    
+    var body: some View {
+        ZStack {
+            switch chagokStatus {
+            case .tutorial:
+                ChagokTutorialView()
+            case .game:
+                ChagokView()
+            case .pause:
+                ChagokPauseView()
+            case .gameover:
+                ChagokGameOverView()
+            }
+        }
+        .onAppear {
+            // 만약 게임이 처음이 아니라면
+            if false {
+                chagokStatus = .game
+            }
+        }
+    }
+}
+
+struct ChagokMainView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChagokMainView(gameSelection: .constant(.chagok))
+    }
+}
---
+++ MC3Team18/MC3Team18/View/Chagok/ChagokPauseView.swift
@@ -0,0 +1,20 @@
+//
+//  SwiftUIView.swift
+//  MC3Team18
+//
+//  Created by ChoiYujin on 2023/07/12.
+//
+
+import SwiftUI
+
+struct ChagokPauseView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        ChagokPauseView()
+    }
+}
