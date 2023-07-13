5785e78c864b5ce83dffcf163bc5140f9ee93efc
MK827 <bluebonnet712@gmail.com>
2023-07-13 12:41:19 +0900

    Feat/#48: 구현 지속시간 카운팅 기능

--- MC3Team18/MC3Team18/View/BubbleGum/BubbleGumMainView.swift
+++ MC3Team18/MC3Team18/View/BubbleGum/BubbleGumMainView.swift
@@ -19,6 +19,11 @@ struct BubbleGumMainView: View {
     @State private var currentImageIndex = 0
     private let images = ["BubbleGumPink","BubbleGumOrange", "BubbleGumBlue"]
     
+    @State var isTimerRunning = false
+    @State private var startTime = Date()
+    @State private var timerString = "0"
+    @State private var timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         
         ZStack(){
@@ -28,6 +33,19 @@ struct BubbleGumMainView: View {
                 .offset(y: backgroundOffset)
                 .animation(.linear(duration: animationMaxDuration), value: backgroundOffset)
             VStack(){
+                //Text((isShowingTutorial || isShowingStartTitle) ? "" : self.timerString)
+                Text(isTimerRunning ? self.timerString : "")
+                    .postNoBillsJaffnaRegular64()
+                    .foregroundColor(.White)
+                    .onReceive(timer) { _ in
+                        if self.isTimerRunning {
+                            timerString = String(format: "%.f", (Date().timeIntervalSince(self.startTime)))
+                        }
+                    }
+                   
+                    .onAppear{
+                        self.stopTimer()
+                    }
                 Text((isShowingTutorial || isShowingStartTitle) ? "" : "소리를 내면 시작합니다")
                     .pretendardSemiBold24()
                     .foregroundColor(.white)
@@ -39,6 +57,15 @@ struct BubbleGumMainView: View {
                     .frame(width: 180, height: 201)
                     .onTapGesture {
                         startGame()
+                        if isTimerRunning {
+                            self.stopTimer()
+                        }
+                        else {
+                            timerString = "0"
+                            startTime = Date()
+                            self.startTimer()
+                        }
+                        isTimerRunning.toggle()
                     }
             }
             .padding(.top, -offsetValue + 161 )
@@ -61,8 +88,17 @@ struct BubbleGumMainView: View {
     
     private func startGame() {
         isShowingStartTitle = true
+        
         scale += 0.8
         backgroundOffset += -offsetValue * 2 - 20
+    }
+    
+    func stopTimer(){
+        self.timer.upstream.connect().cancel()
+    }
+    
+    func startTimer(){
+        self.timer = Timer.publish(every: 0.01, on: .main, in: .common).autoconnect()
     }
 }
 
