7051bc242790e42762d193160b494a326b0987cb
khnueng <khnueng@naver.com>
2023-07-18 12:06:00 +0900

    Feat/#120 차곡차곡 게임에 햅틱효과 추가

---
+++ MC3Team18/MC3Team18/Global/Utils/HapticManager.swift
@@ -0,0 +1,23 @@
+//
+//  HapticManager.swift
+//  MC3Team18
+//
+//  Created by NemoSquare on 7/18/23.
+//
+
+import SwiftUI
+
+class HapticManager {
+    static let instance = HapticManager()
+    private init() {}
+    
+    func notification(type: UINotificationFeedbackGenerator.FeedbackType) {
+        let generator = UINotificationFeedbackGenerator()
+        generator.notificationOccurred(type)
+    }
+    
+    func impact(style: UIImpactFeedbackGenerator.FeedbackStyle) {
+        let generator = UIImpactFeedbackGenerator(style: style)
+        generator.impactOccurred()
+    }
+}
