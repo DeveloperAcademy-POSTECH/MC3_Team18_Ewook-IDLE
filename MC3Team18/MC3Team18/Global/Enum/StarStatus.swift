60a8093a72b0c246311e2d1e2d064c42415f5d18
radin <dlwlsgml8847@gmail.com>
2023-07-23 17:29:38 +0900

    Feat/#184: 추가 StarStatus

---
+++ MC3Team18/MC3Team18/Global/Enum/StarStatus.swift
@@ -0,0 +1,14 @@
+//
+//  StarStatus.swift
+//  MC3Team18
+//
+//  Created by Lee Jinhee on 2023/07/23.
+//
+
+import Foundation
+
+enum StarStatus {
+    case tutorial
+    case game
+    case gameover
+}
