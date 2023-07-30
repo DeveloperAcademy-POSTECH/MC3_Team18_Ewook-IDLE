7421e3b4e55b59360d6ef4c6490b2c62f6eaff33
khnueng <khnueng@naver.com>
2023-07-30 22:46:33 +0900

    Feat/#296 구현 더미 아이템 구입 기능

--- MC3Team18/MC3Team18/View/ShopView.swift
+++ MC3Team18/MC3Team18/View/ShopView.swift
@@ -10,9 +10,11 @@ import SwiftUI
 struct ShopView: View {
     
     @AppStorage("totalCoin") var totalCoin: Int = 1000
+    @AppStorage("dummyItem") var dummyItem: Int = 0
     
     var body: some View {
         VStack{
+            Spacer()
             Text("Shop View")
             Text("Coin Balance: " + String(totalCoin))
             Button(action: {
@@ -26,6 +28,17 @@ struct ShopView: View {
             },
                    label: {
                 Text("Debug : Coin balance--")
+            })
+            Spacer()
+            Text("더미 아이템 갯수 :" + String(dummyItem))
+            Button(action: {
+                if totalCoin >= 100 {
+                    totalCoin = totalCoin - 100
+                    dummyItem += 1
+                }
+            },
+                   label: {
+                Text("더미 아이템 구입, 가격 : 100코인")
             })
         }
     }
