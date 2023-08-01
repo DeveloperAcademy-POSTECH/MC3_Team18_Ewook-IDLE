98862bb6a1979079ce6a6c483400d5f70d0c6250
khnueng <khnueng@naver.com>
2023-08-01 12:54:32 +0900

    Feat/#300 추가 게임오버 화면에 띄울 코인보상 View

---
+++ MC3Team18/MC3Team18/Global/Components/Games/DailyQuestPrizeView.swift
@@ -0,0 +1,18 @@
+//
+//  DailyQuestPrizeView.swift
+//  MC3Team18
+//
+//  Created by NemoSquare on 7/31/23.
+//
+
+import SwiftUI
+
+struct DailyQuestPrizeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DailyQuestPrizeView()
+}
---
+++ MC3Team18/MC3Team18/Global/Components/Games/GameCoinPrizeView.swift
@@ -0,0 +1,18 @@
+//
+//  GameCoinPrizeView.swift
+//  MC3Team18
+//
+//  Created by NemoSquare on 7/31/23.
+//
+
+import SwiftUI
+
+struct GameCoinPrizeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    GameCoinPrizeView()
+}
