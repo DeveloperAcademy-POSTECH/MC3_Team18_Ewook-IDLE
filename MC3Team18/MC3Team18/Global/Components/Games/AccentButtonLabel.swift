91e6e319e6c3ca70d514fad32f9dcf25a5b5dd4b
dayjack <frostjanna12@gmail.com>
2023-08-01 19:28:22 +0900

    UI/#308: 추가 노란색, 주황색 커스텀 버튼 스타일 추가

---
+++ MC3Team18/MC3Team18/Global/Components/Games/AccentButtonLabel.swift
@@ -0,0 +1,35 @@
+//
+//  LightYellowStyleButton.swift
+//  MC3Team18
+//
+//  Created by ChoiYujin on 2023/08/01.
+//
+
+import SwiftUI
+
+struct CustomButtonLabel: View {
+    
+    @State var width: CGFloat = 136
+    @State var height: CGFloat = 96
+    @State var systemName: String = "arrow.clockwise"
+    @State var buttonText: String = "Retry"
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 8) {
+                Image(systemName: "arrow.clockwise")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 24)
+                Text("Retry")
+            }
+        }
+        .frame(width: width, height: height)
+    }
+}
+
+struct LightYellowStyleButton_Previews: PreviewProvider {
+    static var previews: some View {
+        CustomButtonLabel()
+    }
+}
