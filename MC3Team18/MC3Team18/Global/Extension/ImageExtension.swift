37cb2c441db9cad6c03826c330cbec3aa45233d4
radin <dlwlsgml8847@gmail.com>
2023-08-14 00:22:42 +0900

    UI/#328: 추가 ImageExtension backIcon

---
+++ MC3Team18/MC3Team18/Global/Extension/ImageExtension.swift
@@ -0,0 +1,15 @@
+//
+//  ImageExtension.swift
+//  MC3Team18
+//
+//  Created by Lee Jinhee on 2023/08/13.
+//
+
+import SwiftUI
+
+extension Image {
+    static var backIcon: Image {
+         Image(systemName: "chevron.left")
+            .resizable()
+    }
+}
