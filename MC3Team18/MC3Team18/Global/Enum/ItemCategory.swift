2bee3efad0d9fad28f0ffd2437bb2909341d82d7
radin <dlwlsgml8847@gmail.com>
2023-08-14 16:53:24 +0900

    UI/#328: 추가 ItemCategory Enum, 폴더명 변경 Model > MLModel

---
+++ MC3Team18/MC3Team18/Global/Enum/ItemCategory.swift
@@ -0,0 +1,14 @@
+//
+//  ItemCategory.swift
+//  MC3Team18
+//
+//  Created by Lee Jinhee on 2023/08/14.
+//
+
+import Foundation
+
+enum ItemCategory: String, Codable, CaseIterable {
+    case acc = "액세서리"
+    case bubble = "버블버블 풍선"
+    case star = "반짝반짝 별"
+}
